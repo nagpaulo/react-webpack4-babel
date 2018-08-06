600a2d620964bd0f13bce93cbe54aa5ea139461a
paulo.roberto <nagpaulo@gmail.com>
2018-08-06 10:19:06 -0300

    Ajustes

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -4,7 +4,6 @@ import Messages from '../common/msg/messages';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-import '../assets/css/base.css';
 import '../assets/css/bootstrap-drawer.css';
 import '../assets/css/menu.css';
 import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
